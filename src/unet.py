2f46af4f2e2bb224d562fd7a872fb20b85e2ef90
cosmic-cortex <theodore.danka@gmail.com>
2019-01-17 19:21:09 +0100

    2D UNet implementation added

---
+++ src/unet.py
@@ -0,0 +1,185 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.autograd import Variable
+
+
+class First2D(nn.Module):
+    def __init__(self, in_channels, middle_channels, out_channels, dropout=False):
+        super(First2D, self).__init__()
+
+        layers = [
+            nn.Conv2d(in_channels, middle_channels, kernel_size=3, padding=1),
+            nn.BatchNorm2d(middle_channels),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(middle_channels, out_channels, kernel_size=3, padding=1),
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU(inplace=True)
+        ]
+
+        if dropout:
+            assert 0 <= dropout <= 1, 'dropout must be between 0 and 1'
+            layers.append(nn.Dropout2d(p=dropout))
+
+        self.first = nn.Sequential(*layers)
+
+    def forward(self, x):
+        return self.first(x)
+
+
+class Encoder2D(nn.Module):
+    def __init__(
+            self, in_channels, middle_channels, out_channels,
+            dropout=False, downsample_kernel=2
+    ):
+        super(Encoder2D, self).__init__()
+
+        layers = [
+            nn.MaxPool2d(kernel_size=downsample_kernel),
+            nn.Conv2d(in_channels, middle_channels, kernel_size=3, padding=1),
+            nn.BatchNorm2d(middle_channels),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(middle_channels, out_channels, kernel_size=3, padding=1),
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU(inplace=True)
+        ]
+
+        if dropout:
+            assert 0 <= dropout <= 1, 'dropout must be between 0 and 1'
+            layers.append(nn.Dropout2d(p=dropout))
+
+        self.encoder = nn.Sequential(*layers)
+
+    def forward(self, x):
+        return self.encoder(x)
+
+
+class Center2D(nn.Module):
+    def __init__(self, in_channels, middle_channels, out_channels, deconv_channels, dropout=False):
+        super(Center2D, self).__init__()
+
+        layers = [
+            nn.MaxPool2d(kernel_size=2),
+            nn.Conv2d(in_channels, middle_channels, kernel_size=3, padding=1),
+            nn.BatchNorm2d(middle_channels),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(middle_channels, out_channels, kernel_size=3, padding=1),
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU(inplace=True),
+            nn.ConvTranspose2d(out_channels, deconv_channels, kernel_size=2, stride=2)
+        ]
+
+        if dropout:
+            assert 0 <= dropout <= 1, 'dropout must be between 0 and 1'
+            layers.append(nn.Dropout2d(p=dropout))
+
+        self.center = nn.Sequential(*layers)
+
+    def forward(self, x):
+        return self.center(x)
+
+
+class Decoder2D(nn.Module):
+    def __init__(self, in_channels, middle_channels, out_channels, deconv_channels, dropout=False):
+        super(Decoder2D, self).__init__()
+
+        layers = [
+            nn.Conv2d(in_channels, middle_channels, kernel_size=3, padding=1),
+            nn.BatchNorm2d(middle_channels),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(middle_channels, out_channels, kernel_size=3, padding=1),
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU(inplace=True),
+            nn.ConvTranspose2d(out_channels, deconv_channels, kernel_size=2, stride=2)
+        ]
+
+        if dropout:
+            assert 0 <= dropout <= 1, 'dropout must be between 0 and 1'
+            layers.append(nn.Dropout2d(p=dropout))
+
+        self.decoder = nn.Sequential(*layers)
+
+    def forward(self, x):
+        return self.decoder(x)
+
+
+class Last2D(nn.Module):
+    def __init__(self, in_channels, middle_channels, out_channels, softmax=False):
+        super(Last2D, self).__init__()
+
+        layers = [
+            nn.Conv2d(in_channels, middle_channels, kernel_size=3, padding=1),
+            nn.BatchNorm2d(middle_channels),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(middle_channels, middle_channels, kernel_size=3, padding=1),
+            nn.BatchNorm2d(middle_channels),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(middle_channels, out_channels, kernel_size=1),
+            nn.Sigmoid()
+        ]
+
+        self.first = nn.Sequential(*layers)
+
+    def forward(self, x):
+        return self.first(x)
+
+
+class UNet2D(nn.Module):
+    def __init__(self, in_channels, out_channels, conv_depths=[64, 128, 256, 512, 1024]):
+        assert len(conv_depths) > 2, 'conv_depths must have at least 3 members'
+
+        super(UNet2D, self).__init__()
+
+        # defining encoder layers
+        encoder_layers = []
+        encoder_layers.append(First2D(in_channels, conv_depths[0], conv_depths[0]))
+        encoder_layers.extend([Encoder2D(conv_depths[i], conv_depths[i + 1], conv_depths[i + 1])
+                               for i in range(len(conv_depths)-2)])
+
+        # defining decoder layers
+        decoder_layers = []
+        decoder_layers.extend([Decoder2D(2 * conv_depths[i + 1], 2 * conv_depths[i], 2 * conv_depths[i], conv_depths[i])
+                               for i in reversed(range(len(conv_depths)-2))])
+        decoder_layers.append(Last2D(conv_depths[1], conv_depths[0], out_channels))
+
+        # encoder, center and decoder layers
+        self.encoder_layers = nn.Sequential(*encoder_layers)
+        self.center = Center2D(conv_depths[-2], conv_depths[-1], conv_depths[-1], conv_depths[-2])
+        self.decoder_layers = nn.Sequential(*decoder_layers)
+
+    def forward(self, x):
+        x_enc = [x]
+        for enc_layer in self.encoder_layers:
+            x_enc.append(enc_layer(x_enc[-1]))
+
+        x_dec = [self.center(x_enc[-1])]
+        for dec_layer_idx, dec_layer in enumerate(self.decoder_layers):
+            x_opposite = x_enc[-1-dec_layer_idx]
+            x_cat = torch.cat(
+                [pad_to_shape(x_dec[-1], x_opposite.shape), x_opposite],
+                dim=1
+            )
+            x_dec.append(dec_layer(x_cat))
+
+        return x_dec[-1]
+
+
+def pad_to_shape(this, shp):
+    """
+    Pads this tensor with zeroes to shp.
+    Args:
+        this: tensor to pad
+        shp: desired output shape
+
+    Returns:
+        Zero-padded tensor of shape shp.
+    """
+    return F.pad(this, (0, shp[3] - this.shape[3], 0, shp[2] - this.shape[2]))
+
+
+if __name__ == '__main__':
+    device = torch.device('cuda:1')
+    unet = UNet2D(3, 8, conv_depths=[10, 20, 30, 40, 72, 123]).to(device)
+    x = torch.rand(1, 3, 256, 256).to(device)
+    y = unet(x)
+    print(y.shape)
