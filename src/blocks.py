99fd44aed91283bb66b4275ca906e7301c1159a1
cosmic-cortex <theodore.danka@gmail.com>
2019-01-18 08:43:40 +0100

    3D UNet implemented

---
+++ src/blocks.py
@@ -0,0 +1,241 @@
+import torch.nn as nn
+
+
+class First2D(nn.Module):
+    def __init__(self, in_channels, middle_channels, out_channels, dropout=False):
+        super(First2D, self).__init__()
+
+        layers = [
+            nn.Conv2d(in_channels, middle_channels, kernel_size=3, padding=1),
+            nn.BatchNorm2d(middle_channels),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(middle_channels, out_channels, kernel_size=3, padding=1),
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU(inplace=True)
+        ]
+
+        if dropout:
+            assert 0 <= dropout <= 1, 'dropout must be between 0 and 1'
+            layers.append(nn.Dropout2d(p=dropout))
+
+        self.first = nn.Sequential(*layers)
+
+    def forward(self, x):
+        return self.first(x)
+
+
+class Encoder2D(nn.Module):
+    def __init__(
+            self, in_channels, middle_channels, out_channels,
+            dropout=False, downsample_kernel=2
+    ):
+        super(Encoder2D, self).__init__()
+
+        layers = [
+            nn.MaxPool2d(kernel_size=downsample_kernel),
+            nn.Conv2d(in_channels, middle_channels, kernel_size=3, padding=1),
+            nn.BatchNorm2d(middle_channels),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(middle_channels, out_channels, kernel_size=3, padding=1),
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU(inplace=True)
+        ]
+
+        if dropout:
+            assert 0 <= dropout <= 1, 'dropout must be between 0 and 1'
+            layers.append(nn.Dropout2d(p=dropout))
+
+        self.encoder = nn.Sequential(*layers)
+
+    def forward(self, x):
+        return self.encoder(x)
+
+
+class Center2D(nn.Module):
+    def __init__(self, in_channels, middle_channels, out_channels, deconv_channels, dropout=False):
+        super(Center2D, self).__init__()
+
+        layers = [
+            nn.MaxPool2d(kernel_size=2),
+            nn.Conv2d(in_channels, middle_channels, kernel_size=3, padding=1),
+            nn.BatchNorm2d(middle_channels),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(middle_channels, out_channels, kernel_size=3, padding=1),
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU(inplace=True),
+            nn.ConvTranspose2d(out_channels, deconv_channels, kernel_size=2, stride=2)
+        ]
+
+        if dropout:
+            assert 0 <= dropout <= 1, 'dropout must be between 0 and 1'
+            layers.append(nn.Dropout2d(p=dropout))
+
+        self.center = nn.Sequential(*layers)
+
+    def forward(self, x):
+        return self.center(x)
+
+
+class Decoder2D(nn.Module):
+    def __init__(self, in_channels, middle_channels, out_channels, deconv_channels, dropout=False):
+        super(Decoder2D, self).__init__()
+
+        layers = [
+            nn.Conv2d(in_channels, middle_channels, kernel_size=3, padding=1),
+            nn.BatchNorm2d(middle_channels),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(middle_channels, out_channels, kernel_size=3, padding=1),
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU(inplace=True),
+            nn.ConvTranspose2d(out_channels, deconv_channels, kernel_size=2, stride=2)
+        ]
+
+        if dropout:
+            assert 0 <= dropout <= 1, 'dropout must be between 0 and 1'
+            layers.append(nn.Dropout2d(p=dropout))
+
+        self.decoder = nn.Sequential(*layers)
+
+    def forward(self, x):
+        return self.decoder(x)
+
+
+class Last2D(nn.Module):
+    def __init__(self, in_channels, middle_channels, out_channels, softmax=False):
+        super(Last2D, self).__init__()
+
+        layers = [
+            nn.Conv2d(in_channels, middle_channels, kernel_size=3, padding=1),
+            nn.BatchNorm2d(middle_channels),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(middle_channels, middle_channels, kernel_size=3, padding=1),
+            nn.BatchNorm2d(middle_channels),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(middle_channels, out_channels, kernel_size=1),
+            nn.Sigmoid()
+        ]
+
+        self.first = nn.Sequential(*layers)
+
+    def forward(self, x):
+        return self.first(x)
+
+
+class First3D(nn.Module):
+    def __init__(self, in_channels, middle_channels, out_channels, dropout=False):
+        super(First3D, self).__init__()
+
+        layers = [
+            nn.Conv3d(in_channels, middle_channels, kernel_size=3, padding=1),
+            nn.BatchNorm3d(middle_channels),
+            nn.ReLU(inplace=True),
+            nn.Conv3d(middle_channels, out_channels, kernel_size=3, padding=1),
+            nn.BatchNorm3d(out_channels),
+            nn.ReLU(inplace=True)
+        ]
+
+        if dropout:
+            assert 0 <= dropout <= 1, 'dropout must be between 0 and 1'
+            layers.append(nn.Dropout3d(p=dropout))
+
+        self.first = nn.Sequential(*layers)
+
+    def forward(self, x):
+        return self.first(x)
+
+
+class Encoder3D(nn.Module):
+    def __init__(
+            self, in_channels, middle_channels, out_channels,
+            dropout=False, downsample_kernel=2
+    ):
+        super(Encoder3D, self).__init__()
+
+        layers = [
+            nn.MaxPool3d(kernel_size=downsample_kernel),
+            nn.Conv3d(in_channels, middle_channels, kernel_size=3, padding=1),
+            nn.BatchNorm3d(middle_channels),
+            nn.ReLU(inplace=True),
+            nn.Conv3d(middle_channels, out_channels, kernel_size=3, padding=1),
+            nn.BatchNorm3d(out_channels),
+            nn.ReLU(inplace=True)
+        ]
+
+        if dropout:
+            assert 0 <= dropout <= 1, 'dropout must be between 0 and 1'
+            layers.append(nn.Dropout3d(p=dropout))
+
+        self.encoder = nn.Sequential(*layers)
+
+    def forward(self, x):
+        return self.encoder(x)
+
+
+class Center3D(nn.Module):
+    def __init__(self, in_channels, middle_channels, out_channels, deconv_channels, dropout=False):
+        super(Center3D, self).__init__()
+
+        layers = [
+            nn.MaxPool3d(kernel_size=2),
+            nn.Conv3d(in_channels, middle_channels, kernel_size=3, padding=1),
+            nn.BatchNorm3d(middle_channels),
+            nn.ReLU(inplace=True),
+            nn.Conv3d(middle_channels, out_channels, kernel_size=3, padding=1),
+            nn.BatchNorm3d(out_channels),
+            nn.ReLU(inplace=True),
+            nn.ConvTranspose3d(out_channels, deconv_channels, kernel_size=2, stride=2)
+        ]
+
+        if dropout:
+            assert 0 <= dropout <= 1, 'dropout must be between 0 and 1'
+            layers.append(nn.Dropout3d(p=dropout))
+
+        self.center = nn.Sequential(*layers)
+
+    def forward(self, x):
+        return self.center(x)
+
+
+class Decoder3D(nn.Module):
+    def __init__(self, in_channels, middle_channels, out_channels, deconv_channels, dropout=False):
+        super(Decoder3D, self).__init__()
+
+        layers = [
+            nn.Conv3d(in_channels, middle_channels, kernel_size=3, padding=1),
+            nn.BatchNorm3d(middle_channels),
+            nn.ReLU(inplace=True),
+            nn.Conv3d(middle_channels, out_channels, kernel_size=3, padding=1),
+            nn.BatchNorm3d(out_channels),
+            nn.ReLU(inplace=True),
+            nn.ConvTranspose3d(out_channels, deconv_channels, kernel_size=2, stride=2)
+        ]
+
+        if dropout:
+            assert 0 <= dropout <= 1, 'dropout must be between 0 and 1'
+            layers.append(nn.Dropout3d(p=dropout))
+
+        self.decoder = nn.Sequential(*layers)
+
+    def forward(self, x):
+        return self.decoder(x)
+
+
+class Last3D(nn.Module):
+    def __init__(self, in_channels, middle_channels, out_channels, softmax=False):
+        super(Last3D, self).__init__()
+
+        layers = [
+            nn.Conv3d(in_channels, middle_channels, kernel_size=3, padding=1),
+            nn.BatchNorm3d(middle_channels),
+            nn.ReLU(inplace=True),
+            nn.Conv3d(middle_channels, middle_channels, kernel_size=3, padding=1),
+            nn.BatchNorm3d(middle_channels),
+            nn.ReLU(inplace=True),
+            nn.Conv3d(middle_channels, out_channels, kernel_size=1),
+            nn.Sigmoid()
+        ]
+
+        self.first = nn.Sequential(*layers)
+
+    def forward(self, x):
+        return self.first(x)
