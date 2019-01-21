d6ebad12d8834a5cb6fe9922cabca354f91ce411
cosmic-cortex <theodore.danka@gmail.com>
2019-01-21 20:59:55 +0100

    tensor shape problems with long mask fixed

--- unet/metrics.py
+++ unet/metrics.py
@@ -3,19 +3,23 @@ from operator import mul
 from functools import reduce
 
 
-def jaccard_index(output, gt, long_gt=True):
-    if long_gt:
-        gt = torch.zeros_like(output).scatter_(1, gt, 1)
+def jaccard_index(output, gt):
+    """
+    Args:
+        output: torch.Tensor of shape (n_batch, n_classes, n_height, n_width)
+        gt: torch.LongTensor of shape (n_batch, n_height, n_width)
+    """
 
+    gt = torch.zeros_like(output).scatter_(1, gt[:, None, :], 1)
     intersection = output*gt
     union = torch.max(output, gt)
 
     return (intersection.sum().float()/union.sum()).item()
 
 
-def accuracy(output, gt, long_gt=True):
+def accuracy(output, gt):
     output = torch.argmax(output, dim=1, keepdim=True)      # determining the predictions
-    if not long_gt:
-        gt = gt = torch.argmax(gt, dim=1, keepdim=True)
+    correct = (output == gt).sum().float().item()
+    all = reduce(mul, output.shape)
 
-    return ((output == gt).sum().float()/reduce(mul, output.shape)).item()
+    return correct/all
