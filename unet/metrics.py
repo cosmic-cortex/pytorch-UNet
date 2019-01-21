8b4bf76672a75e718805734372a9f9872ae11c4e
cosmic-cortex <theodore.danka@gmail.com>
2019-01-21 15:40:30 +0100

    Jaccard index implemented

--- unet/metrics.py
+++ unet/metrics.py
@@ -1,10 +1,19 @@
-class JaccardIndex:
+import torch
+
+
+def jaccard_index(output, gt, long_gt=True):
+    if long_gt:
+        gt = torch.zeros(output.shape).scatter_(1, gt, torch.ones(output.shape))
+
+    intersection = output*gt
+    union = torch.max(output, gt)
+
+    return intersection.sum()/union.sum()
+
+
+def accuracy(output, gt):
     pass
 
 
-class Accuracy:
-    pass
-
-
-class F1Score:
+def f1_score(output, gt):
     pass
