shape problems during accuracy calculation fixed

unet/metrics.py
@@ -6,8 +6,8 @@ from functools import reduce
 def jaccard_index(output, gt):
     """
     Args:
-        output: torch.Tensor of shape (n_batch, n_classes, n_height, n_width)
-        gt: torch.LongTensor of shape (n_batch, n_height, n_width)
+        output: torch.Tensor of shape (n_batch, n_classes, image.shape)
+        gt: torch.LongTensor of shape (n_batch, image.shape)
     """
 
     gt = torch.zeros_like(output).scatter_(1, gt[:, None, :], 1)
@@ -18,7 +18,13 @@ def jaccard_index(output, gt):
 
 
 def accuracy(output, gt):
-    output = torch.argmax(output, dim=1, keepdim=True)      # determining the predictions
+    """
+    Args:
+        output: torch.Tensor of shape (n_batch, n_classes, image.shape)
+        gt: torch.LongTensor of shape (n_batch, image.shape)
+    """
+
+    output = torch.argmax(output, dim=1)      # determining the predictions
     correct = (output == gt).sum().float().item()
     all = reduce(mul, output.shape)
 