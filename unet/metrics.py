6f453f6eb6b61bdebeb15d5e0d5f6cebbb149151
cosmic-cortex <theodore.danka@gmail.com>
2019-01-23 14:40:05 +0100

    f1_score added

--- unet/metrics.py
+++ unet/metrics.py
@@ -12,7 +12,7 @@ def jaccard_index(output, gt):
 
     gt = torch.zeros_like(output).scatter_(1, gt[:, None, :], 1)
     intersection = output*gt
-    union = torch.max(output, gt)
+    union = output + gt - intersection # torch.max(output, gt)
 
     return (intersection.sum().float()/union.sum()).item()
 
@@ -29,3 +29,24 @@ def accuracy(output, gt):
     all = reduce(mul, output.shape)
 
     return correct/all
+
+
+def f1_score(output, gt, n_classes):
+    """
+    Args:
+        output: torch.Tensor of shape (n_batch, n_classes, image.shape)
+        gt: torch.LongTensor of shape (n_batch, image.shape)
+        n_classes: number of classes
+    """
+
+    epsilon = 1e-20
+
+    output = torch.argmax(output, dim=1)
+    true_positives = torch.tensor([((output == i)*(gt == i)).sum() for i in range(n_classes)]).float()
+    selected = torch.tensor([(output == i).sum() for i in range(n_classes)]).float()
+    relevant = torch.tensor([(gt == i).sum() for i in range(n_classes)]).float()
+
+    precision = (true_positives + epsilon)/(selected + epsilon)
+    recall = (true_positives + epsilon)/(relevant + epsilon)
+
+    return (2*(precision*recall)/(precision + recall)).mean().item()
