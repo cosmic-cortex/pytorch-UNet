fix: softmax and crossentropy loss shenanigans fixed

train.py
@@ -10,7 +10,7 @@ from functools import partial
 from unet.unet import UNet2D
 from unet.model import Model
 from unet.utils import MetricList
-from unet.metrics import jaccard_index, f1_score
+from unet.metrics import jaccard_index, f1_score, LogNLLLoss
 from unet.dataset import Transform2D, ImageToImage2D, Image2D
 
 
@@ -35,8 +35,8 @@ predict_dataset = Image2D(args.val_dataset)
 
 conv_depths = [int(32*(2**k)) for k in range(args.depth)]
 unet = UNet2D(3, 2, conv_depths)
-loss = nn.NLLLoss()
-optimizer = optim.Adam(unet.parameters(), lr=1e-3)
+loss = LogNLLLoss()
+optimizer = optim.Adam(unet.parameters(), lr=1e-4)
 scheduler = optim.lr_scheduler.MultiStepLR(optimizer, gamma=0.1, milestones=[200, 600, 900])
 
 results_folder = os.path.join(args.checkpoint_path, args.model_name)

unet/blocks.py
@@ -123,6 +123,7 @@ class Last2D(nn.Module):
             nn.BatchNorm2d(middle_channels),
             nn.ReLU(inplace=True),
             nn.Conv2d(middle_channels, out_channels, kernel_size=1),
+            nn.Softmax(dim=1)
         ]
 
         self.first = nn.Sequential(*layers)
@@ -141,8 +142,7 @@ class First3D(nn.Module):
             nn.ReLU(inplace=True),
             nn.Conv3d(middle_channels, out_channels, kernel_size=3, padding=1),
             nn.BatchNorm3d(out_channels),
-            nn.ReLU(inplace=True),
-            nn.Softmax2d()
+            nn.ReLU(inplace=True)
         ]
 
         if dropout:
@@ -243,6 +243,7 @@ class Last3D(nn.Module):
             nn.BatchNorm3d(middle_channels),
             nn.ReLU(inplace=True),
             nn.Conv3d(middle_channels, out_channels, kernel_size=1),
+            nn.Softmax(dim=1)
         ]
 
         self.first = nn.Sequential(*layers)