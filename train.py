80328f05ba6d3080b3acf8ea1094ec059e956246
cosmic-cortex <theodore.danka@gmail.com>
2019-02-03 16:35:44 +0100

    add: learning rate argument added

--- train.py
+++ train.py
@@ -24,6 +24,7 @@ parser.add_argument('--epochs', default=100, type=int)
 parser.add_argument('--batch_size', default=1, type=int)
 parser.add_argument('--save_freq', default=0, type=int)
 parser.add_argument('--model_name', type=str, required=True)
+parser.add_argument('--learning_rate', type=float, default=1e-3)
 args = parser.parse_args()
 
 crop = None
@@ -36,8 +37,7 @@ predict_dataset = Image2D(args.val_dataset)
 conv_depths = [int(32*(2**k)) for k in range(args.depth)]
 unet = UNet2D(3, 2, conv_depths)
 loss = LogNLLLoss()
-optimizer = optim.Adam(unet.parameters(), lr=1e-4)
-scheduler = optim.lr_scheduler.MultiStepLR(optimizer, gamma=0.1, milestones=[200, 600, 900])
+optimizer = optim.Adam(unet.parameters(), lr=args.learning_rate)
 
 results_folder = os.path.join(args.checkpoint_path, args.model_name)
 if not os.path.exists(results_folder):
@@ -47,7 +47,7 @@ weights = [0.0, 1.0]
 metric_list = MetricList({'jaccard': partial(jaccard_index, weights=weights),
                           'f1': partial(f1_score, weights=weights)})
 
-model = Model(unet, loss, optimizer, results_folder, scheduler, device=args.device)
+model = Model(unet, loss, optimizer, results_folder, device=args.device)
 
 model.fit_dataset(train_dataset, n_epochs=args.epochs, n_batch=args.batch_size,
                   shuffle=True, val_dataset=val_dataset, save_freq=args.save_freq,
