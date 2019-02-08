51065df147e4a4594e76b3f16e6b6c5c3335e50a
cosmic-cortex <theodore.danka@gmail.com>
2019-02-08 09:38:00 +0100

    weights added to LogNLLLoss

--- unet/metrics.py
+++ unet/metrics.py
@@ -10,13 +10,18 @@ from functools import reduce
 EPSILON = 1e-32
 
 
-class LogNLLLoss(nn.Module):
-    def __init__(self):
-        super(LogNLLLoss, self).__init__()
+class LogNLLLoss(_WeightedLoss):
+    __constants__ = ['weight', 'reduction', 'ignore_index']
 
-    def forward(self, input, target):
-        input = torch.log(input)
-        return cross_entropy(input, target)
+    def __init__(self, weight=None, size_average=None, reduce=None, reduction='mean',
+                 ignore_index=-100):
+        super(LogNLLLoss, self).__init__(weight, size_average, reduce, reduction)
+        self.ignore_index = ignore_index
+
+    def forward(self, y_input, y_target):
+        y_input = torch.log(y_input)
+        return cross_entropy(y_input, y_target, weight=self.weight, reduction=self.reduction,
+                             ignore_index=self.ignore_index)
 
 
 def classwise_iou(output, gt):
